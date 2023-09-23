1c211273c11cae52cfdf90386020e983d6e961ba
borkmeister <borkmaster12@gmail.com>
2023-09-23 03:34:00 +0000

    Add initial bgg api functions

---
+++ src/app/external/bgg_api.py
@@ -0,0 +1,36 @@
+from time import sleep
+
+import requests
+from app.models.BggCollection import BggCollection
+from app.models.BggSearchResultSet import BggSearchResultSet
+from fastapi import HTTPException
+
+
+def get_my_collection() -> BggCollection:
+    collection = None
+    request = requests.get(
+        "https://boardgamegeek.com/xmlapi2/collection?username=borkmeister&subtype=boardgame&own=1"
+    )
+
+    if request.status_code == 200:
+        return BggCollection.from_xml(request.text)
+
+    while request.status_code == 202 and collection is None:
+        sleep(3)
+        collection = get_my_collection()
+
+    if collection:
+        return collection
+    else:
+        raise HTTPException(request.status_code)
+
+
+def search_board_games(query: str) -> BggSearchResultSet:
+    query = query.replace(" ", "+")
+    url = f"https://boardgamegeek.com/xmlapi2/search?query={query}&type=boardgame,boardgameexpansion"
+    request = requests.get(url)
+
+    if request.status_code == 200:
+        return BggSearchResultSet.from_xml(request.text)
+
+    raise HTTPException(request.status_code)
