70052ec51979b46a55e43ae1db91137876b5cc42
borkmeister <borkmaster12@gmail.com>
2023-09-23 03:34:35 +0000

    Add endpoint to return data from bgg api

--- src/app/main.py
+++ src/app/main.py
@@ -4,10 +4,11 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from app.jinja2.templating import get_templates
-from app.routers import html
+from app.routers import bgg, html
 
 app = FastAPI(swagger_ui_parameters={"syntaxHighlight.theme": "obsidian"})
 app.include_router(html.router)
+app.include_router(bgg.router)
 
 
 app.mount("/static", StaticFiles(directory="src/app/static"), name="static")
---
+++ src/app/routers/bgg.py
@@ -0,0 +1,10 @@
+from app.external.bgg_api import get_my_collection
+from app.models.BggCollection import BggCollection
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get("/myboardgames", response_model=BggCollection)
+async def myboardgames() -> BggCollection:
+    return get_my_collection()
