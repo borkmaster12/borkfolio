97150554021d8714b8baed2a2742463e69839c4c
borkmeister <borkmaster12@gmail.com>
2023-09-23 03:32:04 +0000

    Remove redundant endpoints

--- src/app/routers/html.py
+++ src/app/routers/html.py
@@ -23,25 +23,9 @@ async def about(request: Request):
     )
 
 
-@router.get("/videogames", response_class=HTMLResponse)
-async def videogames(request: Request):
-    data = {"page": "Borkfolio - Video Games"}
-    return templates.TemplateResponse(
-        name="videogames.html", context={"request": request, "data": data}
-    )
-
-
 @router.get("/boardgames", response_class=HTMLResponse)
 async def boardgames(request: Request):
     data = {"page": "Borkfolio - Board Games"}
     return templates.TemplateResponse(
         name="boardgames.html", context={"request": request, "data": data}
-    )
-
-
-@router.get("/homelab", response_class=HTMLResponse)
-async def homelab(request: Request):
-    data = {"page": "Borkfolio - Home Lab"}
-    return templates.TemplateResponse(
-        name="homelab.html", context={"request": request, "data": data}
     )
