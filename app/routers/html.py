0100bf381a461cd553f6c6f73c7cf3d00649fa98
borkmeister <borkmaster12@gmail.com>
2023-09-29 02:02:57 +0000

    Add cache header for landing page

--- app/routers/html.py
+++ app/routers/html.py
@@ -1,15 +1,17 @@
-from app.templating import get_templates
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Response
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+
+from app.templating import get_templates
 
 router = APIRouter()
 templates: Jinja2Templates = get_templates()
 
 
 @router.get("/", response_class=HTMLResponse, tags=["html"])
-async def home(request: Request):
+async def home(request: Request, response: Response):
     """Returns the site landing page html."""
+    response.headers["Cache Control"] = "max-age=2592000"
     data = {"page": "Borkfolio - Home"}
     return templates.TemplateResponse(
         name="index.html.j2", context={"request": request, "data": data}
